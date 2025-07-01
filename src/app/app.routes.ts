e278de1eb4a2d8a8bc7ecffefda45157070a3064
Angel Velasquez <pcisavel@upc.edu.pe>
2025-07-01 10:29:52 -0500

    feat(iam): add sign-in and sign-up routes, and authentication guard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
+import {SignInComponent} from './iam/pages/sign-in/sign-in.component';
+import {SignUpComponent} from './iam/pages/sign-up/sign-up.component';
+import {authenticationGuard} from './iam/services/authentication.guard';
 
 const AboutComponent = () => import('./public/pages/about/about.component').then(m => m.AboutComponent);
 const CourseManagementComponent = () => import('./learning/pages/course-management/course-management.component').then(m => m.CourseManagementComponent);
@@ -13,7 +16,9 @@ const baseTitle = 'ACME Learning Center'
 export const routes: Routes = [
   { path: 'home',             component:      HomeComponent,              data: { title: `${baseTitle} | Home`} },
   { path: 'about',            loadComponent:  AboutComponent,             data: { title: `${baseTitle} | About us`} },
-  { path: 'learning/courses', loadComponent:  CourseManagementComponent,  data: { title: `${baseTitle} | Courses`} },
+  { path: 'learning/courses', loadComponent:  CourseManagementComponent,  data: { title: `${baseTitle} | Courses`}, canActivate: [authenticationGuard] },
+  { path: 'sign-in',          component:      SignInComponent,              data: { title: `${baseTitle} | Sign-in`} },
+  { path: 'sign-up',          component:      SignUpComponent,              data: { title: `${baseTitle} | Sign-up`} },
   { path: '',                 redirectTo:     '/home', pathMatch: 'full' },
   { path: '**',               loadComponent:  PageNotFoundComponent,      data: { title: `${baseTitle} | Page not found`} },
 ];
